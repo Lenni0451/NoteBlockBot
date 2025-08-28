356f0859c324f19849716eb687a3d226ecdc94cf
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-08-28 21:15:16 +0200

    Updated NoteBlockTool

--- src/main/java/net/lenni0451/noteblockbot/export/Mp3Encoder.java
+++ src/main/java/net/lenni0451/noteblockbot/export/Mp3Encoder.java
@@ -1,27 +1,27 @@
 package net.lenni0451.noteblockbot.export;
 
 import com.sun.jna.Pointer;
-import net.raphimc.audiomixer.util.io.SoundIO;
+import net.raphimc.audiomixer.util.PcmFloatAudioFormat;
 import net.raphimc.noteblocklib.format.nbs.model.NbsSong;
 import net.raphimc.noteblocktool.audio.SoundMap;
 import net.raphimc.noteblocktool.audio.export.AudioExporter;
 import net.raphimc.noteblocktool.audio.export.LameLibrary;
+import net.raphimc.noteblocktool.audio.export.impl.AudioMixerAudioExporter;
 
-import javax.sound.sampled.AudioFormat;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 
 public class Mp3Encoder {
 
-    private static final AudioFormat FORMAT = new AudioFormat(48000, 16, 2, true, false);
+    private static final PcmFloatAudioFormat FORMAT = new PcmFloatAudioFormat(48000, 2);
 
     public static byte[] encode(final NbsSong song, final File soundsFolder) throws Exception {
         SoundMap.reload(soundsFolder); //Load all custom instruments
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            byte[] samples = sample(song);
-            int numSamples = samples.length / FORMAT.getFrameSize();
-            byte[] mp3Buffer = new byte[(int) (1.25 * numSamples + 7200)];
-            Pointer lame = LameLibrary.INSTANCE.lame_init();
+            float[] samples = sample(song);
+            final int numSamples = samples.length / FORMAT.getChannels();
+            final byte[] mp3Buffer = new byte[(int) (1.25 * numSamples + 7200)];
+            final Pointer lame = LameLibrary.INSTANCE.lame_init();
             if (lame == null) throw new IllegalStateException("Failed to initialize LAME encoder");
             int result = LameLibrary.INSTANCE.lame_set_in_samplerate(lame, (int) FORMAT.getSampleRate());
             if (result < 0) throw new IllegalStateException("Failed to set sample rate: " + result);
@@ -29,7 +29,7 @@ public class Mp3Encoder {
             if (result < 0) throw new IllegalStateException("Failed to set channels: " + result);
             result = LameLibrary.INSTANCE.lame_init_params(lame);
             if (result < 0) throw new IllegalStateException("Failed to initialize LAME parameters: " + result);
-            result = LameLibrary.INSTANCE.lame_encode_buffer_interleaved(lame, samples, numSamples, mp3Buffer, mp3Buffer.length);
+            result = LameLibrary.INSTANCE.lame_encode_buffer_interleaved_ieee_float(lame, samples, numSamples, mp3Buffer, mp3Buffer.length);
             if (result < 0) throw new IllegalStateException("Failed to encode buffer: " + result);
             baos.write(mp3Buffer, 0, result);
             result = LameLibrary.INSTANCE.lame_encode_flush(lame, mp3Buffer, mp3Buffer.length);
@@ -40,10 +40,10 @@ public class Mp3Encoder {
         }
     }
 
-    private static byte[] sample(final NbsSong song) throws Exception {
-        AudioExporter exporter = new MultithreadedAudioMixerAudioExporter(song, FORMAT, 1F, f -> {});
+    private static float[] sample(final NbsSong song) throws Exception {
+        AudioExporter exporter = new AudioMixerAudioExporter(song, FORMAT, 1F, false, true, f -> {});
         exporter.render();
-        return SoundIO.writeSamples(exporter.getSamples(), FORMAT);
+        return exporter.getSamples();
     }
 
 }
