ada2ad2ecc2b037bcf087e168d895f6b240790d3
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-04-08 21:21:48 +0200

    Use variable refresh rate when exporting

--- src/main/java/net/lenni0451/noteblockbot/export/Mp3Encoder.java
+++ src/main/java/net/lenni0451/noteblockbot/export/Mp3Encoder.java
@@ -15,28 +15,42 @@ public class Mp3Encoder {
 
     private static final int MAX_SOUNDS = 16384;
     private static final PcmFloatAudioFormat FORMAT = new PcmFloatAudioFormat(48000, 2);
+    private static final int QUALITY = 50;
 
     public static byte[] encode(final NbsSong song, final File soundsFolder) throws Exception {
         SoundMap.reload(soundsFolder); //Load all custom instruments
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             float[] samples = sample(song);
-            final int numSamples = samples.length / FORMAT.getChannels();
-            final byte[] mp3Buffer = new byte[(int) (1.25 * numSamples + 7200)];
-            final Pointer lame = LameLibrary.INSTANCE.lame_init();
+
+            Pointer lame = LameLibrary.INSTANCE.lame_init();
             if (lame == null) throw new IllegalStateException("Failed to initialize LAME encoder");
             int result = LameLibrary.INSTANCE.lame_set_in_samplerate(lame, (int) FORMAT.getSampleRate());
             if (result < 0) throw new IllegalStateException("Failed to set sample rate: " + result);
             result = LameLibrary.INSTANCE.lame_set_num_channels(lame, FORMAT.getChannels());
             if (result < 0) throw new IllegalStateException("Failed to set channels: " + result);
+            result = LameLibrary.INSTANCE.lame_set_VBR(lame, LameLibrary.vbr_default);
+            if (result < 0) throw new IllegalStateException("Failed to set VBR mode: " + result);
+            result = LameLibrary.INSTANCE.lame_set_VBR_quality(lame, (1F - (QUALITY / 100F)) * 9F);
+            if (result < 0) throw new IllegalStateException("Failed to set VBR quality: " + result);
             result = LameLibrary.INSTANCE.lame_init_params(lame);
             if (result < 0) throw new IllegalStateException("Failed to initialize LAME parameters: " + result);
-            result = LameLibrary.INSTANCE.lame_encode_buffer_interleaved_ieee_float(lame, samples, numSamples, mp3Buffer, mp3Buffer.length);
-            if (result < 0) throw new IllegalStateException("Failed to encode buffer: " + result);
-            baos.write(mp3Buffer, 0, result);
-            result = LameLibrary.INSTANCE.lame_encode_flush(lame, mp3Buffer, mp3Buffer.length);
-            if (result < 0) throw new IllegalStateException("Failed to flush encoder: " + result);
-            baos.write(mp3Buffer, 0, result);
-            LameLibrary.INSTANCE.lame_close(lame);
+
+            int frameCount = samples.length / FORMAT.getChannels();
+            byte[] dataBuffer = new byte[(int) (1.25F * frameCount + 7200)];
+            int dataLength = LameLibrary.INSTANCE.lame_encode_buffer_interleaved_ieee_float(lame, samples, frameCount, dataBuffer, dataBuffer.length);
+            if (dataLength < 0) throw new IllegalStateException("Failed to encode buffer: " + dataLength);
+            byte[] trailerBuffer = new byte[7200];
+            int trailerLength = LameLibrary.INSTANCE.lame_encode_flush(lame, trailerBuffer, trailerBuffer.length);
+            if (trailerLength < 0) throw new IllegalStateException("Failed to flush encoder: " + trailerLength);
+            byte[] headerBuffer = new byte[LameLibrary.INSTANCE.lame_get_lametag_frame(lame, null, 0)];
+            int headerLength = LameLibrary.INSTANCE.lame_get_lametag_frame(lame, headerBuffer, headerBuffer.length);
+            if (headerLength < 0) throw new IllegalStateException("Failed to get LAME tag frame: " + headerLength);
+            result = LameLibrary.INSTANCE.lame_close(lame);
+            if (result < 0) throw new IllegalStateException("Failed to close encoder: " + result);
+
+            baos.write(headerBuffer, 0, headerLength);
+            baos.write(dataBuffer, 0, dataLength);
+            baos.write(trailerBuffer, 0, trailerLength);
             return baos.toByteArray();
         }
     }
